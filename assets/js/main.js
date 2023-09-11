11b9d153c1d34fd8a2591f3ade844919d41cf661
developer-nieem <naimhossenwork@gmail.com>
2023-09-12 00:41:13 +0600

    complete header part

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/* const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const prevButton = document.querySelector(".arrow.prev");
+const nextButton = document.querySelector(".arrow.next");
+let currentIndex = 0;
+
+function updateSlider() {
+    const offset = -currentIndex * (100 / 3);
+    slider.style.transform = `translateX(${offset}%)`;
+}
+
+prevButton.addEventListener("click", () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateSlider();
+    }
+});
+
+nextButton.addEventListener("click", () => {
+    if (currentIndex < slides.length - 3) {
+        currentIndex++;
+        updateSlider();
+    }
+});
+
+updateSlider();
+ */
